dc7eeb9a3f0cba651246b8baa4da7aed93492d50
codewithrabeeh <real.ray48@gmail.com>
2022-06-15 13:06:53 +0530

    configuring session

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
-// const session = require('express-session')
+const session = require('express-session')
 const catchAsync = require('./utils/CatchAsync')
 const ExpressError = require('./utils/ExpressError')
 const Joi = require('joi')
@@ -30,14 +30,18 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// const sessionConfig = {
-//     secret: 'thisshouldbeabettersecret!',
-//     resave: false,
-//     saveUninitialized: true,
-//     // store: 
-// }
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly:true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
 
-// app.use(session(sessionConfig))
+app.use(session(sessionConfig))
 
 
 app.use('/campgrounds', campgrounds)
